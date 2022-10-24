d10636ba3f245033a30393badb1315b85cdda946
Sabbir Alam <sabbir.nit@gmail.com>
2022-10-24 17:48:27 +0800

    Initial commit

---
+++ src/com/sabbir/arrays/A01_isUnique.java
@@ -0,0 +1,13 @@
+package com.sabbir.arrays;
+
+public class A01_isUnique {
+    public static void main(String[] args) {
+        String str = "sabbir";
+        System.out.println(str);
+    }
+
+    public static boolean isUnique(String str){
+        char[] charstr = str.toCharArray();
+        return false;
+    }
+}
