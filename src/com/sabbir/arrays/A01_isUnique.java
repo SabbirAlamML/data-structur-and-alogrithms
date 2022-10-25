ab2f09e1e7f9d2a70d355ab9ebf4ece9ba3b532f
Sabbir Alam <sabbir.nit@gmail.com>
2022-10-25 20:43:23 +0800

    add new function isUnique with char_set

--- src/com/sabbir/arrays/A01_isUnique.java
+++ src/com/sabbir/arrays/A01_isUnique.java
@@ -3,11 +3,40 @@ package com.sabbir.arrays;
 public class A01_isUnique {
     public static void main(String[] args) {
         String str = "sabbir";
-        System.out.println(str);
+        //System.out.println(isUnique1(str));
+        System.out.println(isUnique(str));
+
+    }
+
+    public static boolean isUnique1(String str){
+
+        char[] strArray = str.toCharArray();
+
+        for (int i = 0; i < strArray.length; i++) {
+            for (int j = 1; j < strArray.length-1; j++) {
+                if(i==j){
+                    continue;
+                }
+                if(strArray[i] == strArray[j]){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     public static boolean isUnique(String str){
-        char[] charstr = str.toCharArray();
-        return false;
+        boolean[] char_set = new boolean[128];
+        System.out.println(char_set);
+
+        for (int i = 0; i < str.length(); i++) {
+            int val = str.charAt(i);
+            System.out.println("At index: "+i +" - val: "+val+" char_set[val]: "+  char_set[val]);
+            if(char_set[val]){
+                return false;
+            }
+            char_set[val] = true;
+        }
+        return true;
     }
 }
