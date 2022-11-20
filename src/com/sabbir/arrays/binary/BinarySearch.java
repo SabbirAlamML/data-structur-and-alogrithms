9afe9ee4979f26c52f02e19c237f65b33675f171
Sabbir Alam <sabbir.nit@gmail.com>
2022-11-20 20:39:19 +0800

    Added binary search using while loop and recursion.

---
+++ src/com/sabbir/arrays/binary/BinarySearch.java
@@ -0,0 +1,50 @@
+package com.sabbir.arrays.binary;
+
+public class BinarySearch {
+
+    public static int binarySearchWhile(int[] inputArray, int key) {
+        int start = 0;
+        int end = inputArray.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (inputArray[mid] == key) {
+                return mid;
+            }
+            if (inputArray[mid] < key) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int binarySearchRecursion(int[] inputArray, int start, int end, int key) {
+        if (end >= start) {
+            int mid = (start + end) / 2;
+            if (inputArray[mid] == key) {
+                return mid;
+            }
+            if (inputArray[mid] < key) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+            return binarySearchRecursion(inputArray, start, end, key);
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] inputArray = {2, 3, 4, 5, 6, 7, 8};
+        System.out.println(binarySearchWhile(inputArray, 6));
+        System.out.println(binarySearchWhile(inputArray, 3));
+        System.out.println(binarySearchWhile(inputArray, 9));
+
+        System.out.println(binarySearchRecursion(inputArray, 0, 6, 6));
+        System.out.println(binarySearchRecursion(inputArray, 0,6,3));
+        System.out.println(binarySearchRecursion(inputArray, 0, 6,9));
+
+    }
+}
